d34bd6fadb36291a87d029c253fa729839c07019
Ewerton Madruga <emadruga@gmail.com>
2017-07-19 16:04:21 -0300

    implemented list of sliding items, which lead to its subitems if pressed upon, and on slide can be deleted.

--- src/providers/data/data.ts
+++ src/providers/data/data.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 /*
@@ -11,7 +10,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class DataProvider {
 
-  constructor(public http: Http) {
+  constructor() {
     console.log('Hello DataProvider Provider');
   }
 
